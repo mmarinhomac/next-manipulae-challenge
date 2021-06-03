e243475d7d3d837b878d48bb39af96b88faface8
Matheus Teixeira <marinhoteix@gmail.com>
2021-06-03 01:29:32 -0300

    fix: deploy flavicon inconsistency

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,8 +11,8 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&display=swap" rel="stylesheet" />
 
           <link rel="apple-touch-icon" href="icons/icon-192x192.png" />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+          <link rel="icon" href="/favicon.png" type="image/png" />
           <link rel='manifest' href='/manifest.json' />
         </Head>
         <body>
